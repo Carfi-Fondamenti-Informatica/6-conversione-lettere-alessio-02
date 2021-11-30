3f44430059b575cb21df133d87eec839b61efe14
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-11-30 16:23:37 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,14 @@
 
 #include "lib.h"
+
+bool verifica(char z) {
+    if (z>=65 and 90>=z or z>=97 and 122>=z){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 char conv(char &let) {
     if (let>=65 and 90>=let){
         let=let+32;
