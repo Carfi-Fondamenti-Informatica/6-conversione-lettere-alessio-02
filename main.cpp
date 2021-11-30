dd5092313d4ffae30510a4c4ec7326aac4d7b114
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-11-30 16:32:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "lib.cpp"
+#include "lib.h"
 using namespace std;
 
 int main() {
     char let = 'a';
     cin >> let;
     if(verifica(let)==true){
-        cout << conv(let);
+        cout << conv(let) <<endl;
     }
     if(verifica(let)==false){
         cout << "errore" <<endl;
