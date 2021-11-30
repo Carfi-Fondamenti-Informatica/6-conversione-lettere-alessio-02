bb13ef05b63ea0dbb29bf8c44ae439de3da16dce
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-11-30 16:22:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,13 @@
 #include "lib.cpp"
 using namespace std;
 
-char let = 'a';
-bool v;
-
-void verifica(char z) {
-    if (z>=65 and 90>=z or z>=97 and 122>=z){
-        v= true;
-    }else{
-        v= false;
-    }
-}
-
 int main() {
+    char let = 'a';
     cin >> let;
-    verifica(let);
-    if(v== true){
+    if(verifica(let)==true){
         cout << conv(let);
     }
-    if(v== false){
+    if(verifica(let)==false){
         cout << "errore" <<endl;
     }
     return 0;
