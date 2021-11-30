2efc2e416764847daa9e885ea62c3d5bbb04f1f2
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-11-30 16:04:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
 
+#include "lib.h"
+char conv(char &let) {
+    if (let>=65 and 90>=let){
+        let=let+32;
+    }
+    else if (let>=97 and 122>=let){
+        let=let-32;
+    }
+    return let;
+}
